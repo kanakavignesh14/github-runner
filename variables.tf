14f9d96d6c696a0c1975bb4afcb435bda772864b
kanakavignesh14 <201001039@rajalakshmi.edu.in>
2026-04-13 19:29:40 +0530

    create-runner

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "vigi-devops.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0286242NWJQZPBB72EL"
+  description = "description"
+}
